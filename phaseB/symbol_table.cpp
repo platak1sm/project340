d9b3937c6587569a410427ca14daec8db1bc755e
Irene Patsoura <irene.patsoura@gmail.com>
2023-03-24 17:56:36 +0200

    Symbol table implementation

--- phaseB/symbol_table.cpp
+++ phaseB/symbol_table.cpp
@@ -8,7 +8,37 @@ void insert(SymbolTableEntry ste){
 
 SymbolTableEntry lookup(string s, int scope) {
     for(unsigned int i = 0; i < SymbolTable.size(); i++) {
-        if(SymbolTable[i].type ==  ||
+        if(SymbolTable[i].type == USERFUNC || SymbolTable[i].type == LIBFUNC) {
+            if(SymbolTable[i].funcVal.name == s) {
+                if(SymbolTable[i].funcVal.scope == scope)
+                    return SymbolTable[i];
+            }
+        }
+        else if(SymbolTable[i].type == GLOBAL || SymbolTable[i].type == LOCAL  || SymbolTable[i].type == FORMAL) {
+            if(SymbolTable[i].varVal.name == s) {
+                if(SymbolTable[i].varVal.scope == scope)
+                    return SymbolTable[i];
+            }
+        }
+    }
+
+    SymbolTableEntry unknown;
+    unknown.isActive = false;
+    return unknown;
+                
+}
+
+void hide(int scope) {
+    for(unsigned int i = 0; i < SymbolTable.size(); i++) {
+        if(SymbolTable[i].type == USERFUNC || SymbolTable[i].type == LIBFUNC || SymbolTable[i].type == GLOBAL || SymbolTable[i].type == LOCAL  || SymbolTable[i].type == FORMAL) {
+            if(SymbolTable[i].funcVal.scope >= scope) {
+                SymbolTable[i].isActive = false;
+            }
+        }
+        else if(SymbolTable[i].type == GLOBAL || SymbolTable[i].type == LOCAL  || SymbolTable[i].type == FORMAL) {
+            if(SymbolTable[i].varVal.scope >= scope) {
+                SymbolTable[i].isActive = false;
+            }
+        }
     }
 }
-void hide();
